696a6e11bf2a8b498ad55910b626bc6548f16eb0
bert-bae <elbert.bae@gmail.com>
2023-09-25 13:20:49 -0700

    refactor to abstract everything from game impl expect new and start

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ struct Game {
 
 static BOARD_WIDTH: usize = 9 * 3;
 impl Game {
-    pub fn new(tiles: [Tile; 9], players: (String, String)) -> Self {
+    fn new(tiles: [Tile; 9], players: (String, String)) -> Self {
         Game {
             board: tiles,
             players,
@@ -21,6 +21,33 @@ impl Game {
         }
     }
 
+    fn start(&mut self) {
+        self.next();
+        println!(
+            "Lets start the game! {} goes first.",
+            self.get_current_player()
+        );
+        let mut complete = false;
+        while !complete {
+            let valid = self.validate();
+            if valid {
+                complete = true;
+            } else {
+                self.draw();
+
+                let _ = stdout().flush();
+                let mut tile = String::new();
+
+                println!("It's {}'s turn...", self.get_current_player());
+                stdin().read_line(&mut tile).unwrap();
+
+                match self.select_tile(&tile.trim()) {
+                    Ok(_) => self.next(),
+                    Err(e) => println!("Invalid selection: {e}"),
+                }
+            }
+        }
+    }
     fn draw_into(&self, buffer: &mut dyn std::fmt::Write) {
         let mut row_counter = 0;
         let column_width = 7;
@@ -54,17 +81,17 @@ impl Game {
         }
     }
 
-    pub fn draw(&self) {
+    fn draw(&self) {
         let mut buffer = String::new();
         self.draw_into(&mut buffer);
         println!("{buffer}");
     }
 
-    pub fn validate(&self) -> bool {
+    fn validate(&self) -> bool {
         return false;
     }
 
-    pub fn get_current_player(&self) -> &String {
+    fn get_current_player(&self) -> &String {
         if self.next.is_none() || self.next.unwrap() == 0 {
             &self.players.0
         } else {
@@ -72,7 +99,7 @@ impl Game {
         }
     }
 
-    pub fn next(&mut self) {
+    fn next(&mut self) {
         let next_player = match self.next {
             None => 0,
             Some(0) => 1,
@@ -83,7 +110,7 @@ impl Game {
         self.next = Some(next_player);
     }
 
-    pub fn select_tile(&mut self, key: &str) -> Result<bool, &str> {
+    fn select_tile(&mut self, key: &str) -> Result<bool, &str> {
         let position = self.board.iter().position(|t| t.get_key() == key);
         match position {
             Some(pos) => {
@@ -103,12 +130,11 @@ impl Game {
     }
 }
 
-fn start_game() {
+fn main() {
     let tile_keys = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
     let tiles = tile_keys.map(|key| Tile::new(key.to_string()));
 
     let players = (String::from("Player1"), String::from("Player2"));
-    let mut game = Game::new(tiles, players);
 
     let mut p1 = String::new();
     let mut p2 = String::new();
@@ -120,31 +146,6 @@ fn start_game() {
     let _ = stdout().flush();
     stdin().read_line(&mut p2).unwrap();
 
-    println!("Lets start the game! {} goes first.", p1);
-
-    game.next();
-    let mut complete = false;
-    while !complete {
-        let valid = game.validate();
-        if valid {
-            complete = true;
-        } else {
-            game.draw();
-
-            let _ = stdout().flush();
-            let mut tile = String::new();
-
-            println!("It's {}'s turn...", game.get_current_player());
-            stdin().read_line(&mut tile).unwrap();
-
-            match game.select_tile(&tile.trim()) {
-                Ok(_) => game.next(),
-                Err(_) => println!("Tile {tile} is out of bounds"),
-            }
-        }
-    }
-}
-
-fn main() {
-    start_game();
+    let mut game = Game::new(tiles, players);
+    game.start();
 }
